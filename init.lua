4bdf878529ddb4fcc359480ac50378706a8b152e
AlvarSanMartin <alvar.sanmartin@dominion-global.com>
2025-08-26 14:13:22 +0200

    atajos de teclado

--- init.lua
+++ init.lua
@@ -5,4 +5,5 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-vim.keymap.set('n', "<C-b>", ":Neotree toggle filesystem reveal right <CR>")
+-- neotree
+vim.keymap.set('n', "<C-b>", "<Cmd>Neotree toggle<CR>")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = LazyVim.safe_keymap_set
+
+-- Neotree
+map('n', "<C-b>", ":Neotree toggle<CR>", { desc = "Toggle explorador" })
+map('n', "<leader>B", ":Neotree source=buffers<CR>", { desc = "Buffers abiertos" })
+
+-- Tabs
+map('n', "<leader><tab><tab>", ":tabNext<CR>", { desc = "Next tab" })
+map('n', "<leader><tab>n", ":tabnew<CR>", { desc = "New tab" })
+
+-- Terminal
+map('t', '<esc><esc>', [[<C-\><C-n>]])
